0f24b7e286d3df34ce12ef116b32522c97bb224d
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2019-03-15 12:07:00 +0100

    fix formatting

--- Assets/Scripts/AutoResourceHandler.cs
+++ Assets/Scripts/AutoResourceHandler.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using UnityEditor;
 
-    public class AutoResourceHandler
+public class AutoResourceHandler
+{
+    public AutoResourceHandler()
     {
-        public AutoResourceHandler()
-        {
-        }
     }
+}
 
+public class ClassDescriptor
+{
+    public string saveLocation;
+    public string namespaceName;
+    public string className;
+    public string EnumName { get { return string.Format("{0}Enum", className); } }
+    public string MapperName { get { return string.Format("{0}Mapper", className); } }
 
-    public class ClassDescriptor
-    {
-        public string saveLocation;
-        public string namespaceName;
-        public string className;
-        public string EnumName { get { return string.Format("{0}Enum", className); } }
-        public string MapperName { get { return string.Format("{0}Mapper", className); } }
-
-        public List<SelectedItem> items = new List<SelectedItem>();
-    }
+    public List<SelectedItem> items = new List<SelectedItem>();
+}
 
 public class SelectedItem
 {
