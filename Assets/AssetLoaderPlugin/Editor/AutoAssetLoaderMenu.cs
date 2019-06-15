585a0617450b33d8b81d0a9d37d2055c815971ac
behrooz torki <behrooz.mcsd@gmail.com>
2019-06-15 12:55:39 +0200

    adds more documentation

--- Assets/AssetLoaderPlugin/Editor/AutoAssetLoaderMenu.cs
+++ Assets/AssetLoaderPlugin/Editor/AutoAssetLoaderMenu.cs
@@ -69,12 +69,27 @@ namespace AutoAssetLoader
             
         }
 
+        /// <summary>
+        /// Creates a button and call its call back when pressed
+        /// </summary>
+        /// <param name="label">button label</param>
+        /// <param name="buttonRect">rectangle describing the display area for the button</param>
+        /// <param name="action">callback to be called when button is pressed</param>
         void CreateButton(string label, Rect buttonRect, Action action)
         {
             if (GUI.Button(buttonRect, label))
                 action?.Invoke();
         }
 
+        /// <summary>
+        /// Creates a textfield and returns its edited value
+        /// </summary>
+        /// <param name="label">label to show on the left of textfield</param>
+        /// <param name="value">current value of the field</param>
+        /// <param name="rect">rectangle representing the display area of label and textfield</param>
+        /// <param name="inputMargin">horizental margin between label and textfield</param>
+        /// <param name="dragDrop">describes whether field accepts drag and drop or not</param>
+        /// <returns>new value of the text field</returns>
         string CreateTextInput(string label, string value, Rect rect, float inputMargin = 20, bool dragDrop = false)
         {
             Rect labelRect = new Rect(rect.xMin, rect.yMin, rect.width, rect.height);
@@ -102,6 +117,15 @@ namespace AutoAssetLoader
             return value;
         }
 
+        /// <summary>
+        /// Creates a drop down to select a value from an enum and return the selected value
+        /// </summary>
+        /// <typeparam name="T">requested enum type</typeparam>
+        /// <param name="label">label to show on the left of dropdown</param>
+        /// <param name="selectedEntry">currently selected value</param>
+        /// <param name="rect">rectangle representing the display area of label and dropdown</param>
+        /// <param name="inputMargin">horizental margin between label and dropdown</param>
+        /// <returns>selected value from the enum</returns>
         T CreateEnumDropdown<T>(string label, T selectedEntry, Rect rect, float inputMargin = 20)
         {
             Rect labelRect = new Rect(rect.xMin, rect.yMin, rect.width, rect.height);
